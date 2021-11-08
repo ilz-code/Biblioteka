4a9c8df29e93d84f53f1183f78d4f5e2eb6ca729
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-11-08 16:09:19 +0200

    Atbildes

--- Biblioteka/Data/Searches.cs
+++ Biblioteka/Data/Searches.cs
@@ -29,28 +29,17 @@ namespace Biblioteka.Data
             var res = context.Gramatas.FromSqlRaw(
                 "SELECT Gramatas.Nosaukums as Nosaukums, Autors.Vards as Vards " +
                 "FROM Gramatas " +
-                "JOIN Autors On Autors.Id = Gramatas.AutoraId" +
+                "JOIN Autors On Autors.Id = Gramatas.AutoraId " +
                 "WHERE Autors.Vards = 'A. Konans Doils'");
+
+            Console.WriteLine("Nosaukums\t\tAutors");
             Console.WriteLine(res);
-            Console.WriteLine();
-            //var res = from g in context.Gramatas
-            //    join a in context.Autors 
-            //        on g.AutoraId equals a.Id into dept
-            //    from department in dept.DefaultIfEmpty()
-            //    select new
-            //    {
-            //        Nosaukums = g.Nosaukums,
-            //        Autors = a.
-            //        Gads = g.Gads,
-            //        Lappuses = g.Lpp;
-            //    };
-            //Console.WriteLine("Employee Code\tEmployee Name\tDepartment Name");
-            //foreach (var data in leftOuterJoin)
+            //foreach (var gr in res)
             //{
-            //    Console.WriteLine(data.EmployeeCode + "\t\t" + data.EmployeeName + "\t" + data.DepartmentName);
+            //    Console.WriteLine(gr.Nosaukums);
             //}
-            //context.Gramatas;
-            //return res;
+            Console.WriteLine();
+            
         }
     }
 }
---
+++ Biblioteka/Models/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteka.Models
+{
+    public class Result
+    {
+        public int Id { get; set; }
+        public string Nosaukums { get; set; }
+        //public string Autors { get;set }
+    }
+}
