a2793d3ed2570b43a86c9e75fc195aecbb96b9b8
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-11-08 09:18:38 +0200

    Project

---
+++ Biblioteka/Models/Gramata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteka.Models
+{
+    public class Gramata
+    {
+        public int Id { get; set; }
+        public string Nosaukums { get; set; }
+        public string AutoraId { get; set; }
+        public int Gads { get; set; }
+        public int Lpp { get; set; }
+    }
+}
